28614def5a9a8f58a4713fcfd97e03b6dc3ab3b4
matthewlabenznws <matthew.labenz@noaa.gov>
2026-05-10 12:34:04 -0500

    Update rrfs_hail_swath.py

--- rrfs_hail_swath.py
+++ rrfs_hail_swath.py
@@ -208,7 +208,9 @@ add_spc_severe_domain()
 # SETTINGS
 # ============================================================
 
-VALID_RRFS_CYCLES = [0, 3, 6, 9, 12, 15, 18, 21]
+VALID_RRFS_CYCLES = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
+                     21, 22, 23]
+
 
 START_FHR = 1
 CYCLE_DELAY_MINUTES = 75
